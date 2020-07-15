ffe1c997decd0d46dad78c61cae31d99119be6b0
mcfinley <mcfinley@yandex.ru>
2020-07-15 17:12:14 +0300

    Install mongoose

---
+++ server/src/libs/mongoose.ts
@@ -0,0 +1,10 @@
+import mongoose from 'mongoose'
+
+const URL = `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}:${process.env.MONGO_PORT}`
+
+mongoose.connect(URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+
+export default mongoose

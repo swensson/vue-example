4557cace27f8a427c75853fa558c33912a735b62
mcfinley <mcfinley@yandex.ru>
2020-07-15 16:53:13 +0300

    Sketch of clients router

--- server/src/index.ts
+++ server/src/index.ts
@@ -10,6 +10,17 @@ app.get('/health', (req, res) => {
   res.sendStatus(200)
 })
 
+/**
+ * Simple error middleware
+ */
+app.use((err, req, res, next) => {
+  if (!!err.code) {
+    res.status(err.code).send(err.toString())
+  } else {
+    res.status(500).send(err.toString())
+  }
+})
+
 app.listen(process.env.SERVER_PORT, () => {
   console.log('Server is listening at', process.env.SERVER_PORT)
 })
---
+++ server/src/routes/clients.ts
@@ -0,0 +1,40 @@
+import express from 'express'
+
+const router = express.Router()
+
+/**
+ * Create a client
+ */
+router.post('/', (req, res) => {
+
+})
+
+/**
+ * Get all clients
+ */
+router.get('/', (req, res) => {
+
+})
+
+/**
+ * Get one client
+ */
+router.get('/:id', (req, res) => {
+
+})
+
+/**
+ * Update a client
+ */
+router.put('/:id', (req, res) => {
+
+})
+
+/**
+ * Delete a client
+ */
+router.delete('/:id', (req, res) => {
+
+})
+
+export default router

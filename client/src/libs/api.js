effe04dd8b12b8278f7485c67dc85c7477cbf064
mcfinley <mcfinley@yandex.ru>
2020-07-17 13:59:06 +0300

    Improve api

--- client/src/libs/api.js
+++ client/src/libs/api.js
@@ -1,8 +1,15 @@
 import axios from 'axios'
 
-const instance = axios.create({ baseURL: `http://${process.env.SERVER_HOST}:${process.env.SERVER_PORT}` })
+const instance = axios.create({
+  baseURL: `http://${process.env.SERVER_HOST}:${process.env.SERVER_PORT}`,
+  headers: { 'Accept': 'application/json', 'Content-type': 'application/json' },
+})
+
+instance.interceptors.response.use((response) => response.data)
 
 export default {
-  getClients: () =>
-    instance.get('/clients').then(({ data }) => data),
+  getClients: () => instance.get('/clients'),
+  getProviders: () => instance.get('/providers'),
+  createProvider: (body) => instance.post('/providers', body),
+  removeProvider: (id) => instance.delete(`/providers/${id}`),
 }
